11f4151261e695c7821ac6b046b212d200598ef1
kunalkeshan <kunalkeshan12@gmail.com>
2021-09-26 10:25:56 +0530

    Remove and show modal functionality, and resolved font-awesome icon issue, just add font family in body instead of *(all)

--- src/app.js
+++ src/app.js
@@ -1,44 +1,94 @@
 document.addEventListener("DOMContentLoaded", () => {
+    //Form Elements
     const addTaskForm = document.getElementById("addTaskForm");
     const searchTaskForm = document.getElementById("searchTaskForm");
 
+    //Task List
+    const taskList = document.getElementById("taskList");
+
+    //Task List Items
+    let taskItems = document.querySelectorAll(".taskItem");
+    let taskEditBtn = document.querySelectorAll(".taskItem .fa-edit ")
+    let taskDeleteBtn = document.querySelectorAll(".taskItem .fa-trash-alt")
+
+    //Edit Task Modal Elements
     const editTaskModal = document.getElementById("taskEditModal");
     const editTaskBtn = document.getElementById("editBtn");
     const closeEditTaskBtn = document.getElementById("closeBtn");
+
+    //How to Use To-Do list Elements
     const howToUseModal = document.getElementById("howToUse");
     const howToUseLink = document.getElementById("howToUseLink");
     const howToUseCloseBtn = document.getElementById("htuBtn");
     const overlay = document.getElementById("overlay");
 
-
-    // Add JS functionality when list items length 
-    // is more than the height of the ul then make 
-    // overflow-y as scroll
+    //Global Modal Functions
+    //Function to display the edit task modal
     function showEditTaskModal(){
-
+        editTaskModal.classList.toggle("add-in");
+        editTaskModal.style.display = "flex";
+        overlay.style.display = "block";
     }
 
+    //Function to display the how to use to do list modal
     function showHowToUseModal(){
-
+        howToUseModal.classList.toggle("add-in");
+        howToUseModal.style.display = "block";
+        overlay.style.display = "block";
     }
 
-    function HideAllModal(){
+    //Common function to hide all modals and overlay
+    function HideAllModals(){
+        howToUseModal.classList.toggle("add-in");
+        editTaskModal.classList.toggle("add-in");
         editTaskModal.style.display = "none";
         howToUseModal.style.display = "none";
         overlay.style.display = "none";
     }
 
+    //Function to check list height, and add scroll 
+    //if item height is more than list height
+    function checkListHeight(){
+        taskItems = document.querySelectorAll(".taskItem");
+        const listHeight = taskList.offsetHeight;
+        let itemsHeight = 0;
+        taskItems.forEach((item) => {
+            let marginTop = getComputedStyle(item).marginTop;
+            let marginBottom = getComputedStyle(item).marginBottom
+            let marginY = parseInt(marginTop) + parseInt(marginBottom);
+            itemsHeight += item.offsetHeight + marginY;
+        })
+        if(itemsHeight > listHeight){
+            taskList.style.overflowY = "scroll";
+        }
+    }
+
+    checkListHeight()
+
+
+    //Event listeners to invoke functions to display
+    //and hide the respective modals
+
+    taskEditBtn.forEach((editBtn) => {
+        editBtn.addEventListener("click", () => {
+            showEditTaskModal();
+        });
+    });
+
+    closeEditTaskBtn.addEventListener("click", () => {
+        HideAllModals();
+    });
+
     howToUseLink.addEventListener("click", () =>{
-        howToUseModal.style.display="block";
-        overlay.style.display="block";
+        showHowToUseModal();
     })
 
     howToUseCloseBtn.addEventListener("click", () =>{
-        HideAllModal();
+        HideAllModals();
     })
 
     overlay.addEventListener("click", () =>{
-        HideAllModal();
+        HideAllModals();
     })
 
 
